9b66bd47c4c5c1a297811bb1ae76ace8043a7b0f
Caleb Stewart <calebstew32@hotmail.com>
2025-03-19 15:39:09 -0700

    Now displays confidence level

--- src/components/ResultDetails.jsx
+++ src/components/ResultDetails.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+
 const ResultDetails = ({ boundingBoxInfo }) => {
     if (boundingBoxInfo.length === 0) {
         return null
     }
 
+    const confidenceScores = { 2: 'Low', 3: 'Medium', 4: 'High' }
+
+    console.log(boundingBoxInfo)
     // Show all of the logos with bounding boxes cropped
     return (
         <div className="w-l bg-base-200 border border-base-200 p-6 rounded-box justify-center mb-4">
@@ -19,11 +23,17 @@ const ResultDetails = ({ boundingBoxInfo }) => {
                             style={{ maxWidth: '100%', maxHeight: '200px' }}
                         />
                         <div className="text-base">
-                            {' '}
                             {/* Text of how much of the image is taken by the logo */}
                             This logo takes {box.box_coverage_percentage}% of
-                            the main image
+                            the main image.
                         </div>
+
+                        {box.confidence && ( // If the confidence score is available, display it (only for specific logo detection)
+                            <div className="text-base">
+                                Confidence of:{' '}
+                                {confidenceScores[box.confidence]}
+                            </div>
+                        )}
                     </div>
                 ),
             )}
