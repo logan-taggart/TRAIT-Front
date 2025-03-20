89d585204143bcf55604ae0b9e83a9ebb8edc387
Caleb Stewart <calebstew32@hotmail.com>
2025-03-20 10:44:48 -0700

    Changed title of application

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import { app, BrowserWindow } from "electron";
-import { spawn } from "child_process";
-import path from "path";
-import { fileURLToPath } from "url";
+import { app, BrowserWindow } from 'electron';
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Manually define __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -10,17 +10,17 @@ const __dirname = path.dirname(__filename);
 let mainWindow;
 let backendProcess;
 
-app.on("ready", () => {
-    console.log("Starting Flask backend...");
+app.on('ready', () => {
+    console.log('Starting Flask backend...');
 
-    backendProcess = spawn("python", ["run.py"], {
-        cwd: path.join(__dirname, "backend"),
+    backendProcess = spawn('python', ['run.py'], {
+        cwd: path.join(__dirname, 'backend'),
         shell: true,
         detached: false,
-        stdio: "inherit"
+        stdio: 'inherit',
     });
 
-    console.log("Launching Electron window...");
+    console.log('Launching Electron window...');
 
     mainWindow = new BrowserWindow({
         width: 800,
@@ -30,33 +30,33 @@ app.on("ready", () => {
         },
     });
 
-    mainWindow.loadURL("http://localhost:5173");
+    mainWindow.loadURL('http://localhost:5173');
 
-    mainWindow.on("closed", () => {
-        console.log("Electron window closed");
+    mainWindow.on('closed', () => {
+        console.log('Electron window closed');
         mainWindow = null;
     });
 
-    mainWindow.on("close", () => {
-        console.log("Closing Electron...");
+    mainWindow.on('close', () => {
+        console.log('Closing Electron...');
         if (backendProcess) {
-            console.log("Sending SIGTERM to backend...");
-            backendProcess.kill("SIGTERM");
+            console.log('Sending SIGTERM to backend...');
+            backendProcess.kill('SIGTERM');
         }
     });
 });
 
-app.on("quit", () => {
-    console.log("Electron is quitting...");
+app.on('quit', () => {
+    console.log('Electron is quitting...');
     if (backendProcess) {
-        console.log("Sending SIGTERM to backend...");
-        backendProcess.kill("SIGTERM");
+        console.log('Sending SIGTERM to backend...');
+        backendProcess.kill('SIGTERM');
     }
 });
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        console.log("All windows closed, quitting Electron...");
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        console.log('All windows closed, quitting Electron...');
         app.quit();
     }
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,82 @@
-import React, { useState } from 'react'
-import UploadSection from './components/UploadSection'
-import DetectionOptions from './components/DetectionOptions'
-import ResultDisplay from './components/ResultDisplay'
-import './App.css'
+import React, { useState } from 'react';
+import UploadSection from './components/UploadSection';
+import DetectionOptions from './components/DetectionOptions';
+import ResultDisplay from './components/ResultDisplay';
+import './App.css';
 
 function App() {
     const baseURL =
         window.location.hostname === 'localhost'
             ? 'http://127.0.0.1:5174'
-            : 'https://trademark-identification-back.onrender.com'
+            : 'https://trademark-identification-back.onrender.com';
 
-    const [mainFile, setMainFile] = useState(null)
-    const [referenceFile, setReferenceFile] = useState(null)
-    const [detectionMode, setDetectionMode] = useState('all')
-    const [confidenceThreshold, setConfidenceThreshold] = useState(3)
-    const [boundingBoxThreshold, setBoundingBoxThreshold] = useState(50)
-    const [imageUrl, setImageUrl] = useState('None')
-    const [resultMessage, setResultMessage] = useState('')
-    const [boundingBoxInfo, setBoundingBoxInfo] = useState([])
+    const [mainFile, setMainFile] = useState(null);
+    const [referenceFile, setReferenceFile] = useState(null);
+    const [detectionMode, setDetectionMode] = useState('all');
+    const [confidenceThreshold, setConfidenceThreshold] = useState(3);
+    const [boundingBoxThreshold, setBoundingBoxThreshold] = useState(50);
+    const [imageUrl, setImageUrl] = useState('None');
+    const [resultMessage, setResultMessage] = useState('');
+    const [boundingBoxInfo, setBoundingBoxInfo] = useState([]);
 
     // Default white
-    const [selectedBBColor, setSelectedBBColor] = useState('#FFFFFF')
+    const [selectedBBColor, setSelectedBBColor] = useState('#FFFFFF');
 
     const handleSubmit = async () => {
         if (!mainFile || (detectionMode === 'specific' && !referenceFile)) {
-            setResultMessage('Please upload the required images.')
-            return
+            setResultMessage('Please upload the required images.');
+            return;
         }
 
-        const formData = new FormData()
-        formData.append('main_image', mainFile)
+        const formData = new FormData();
+        formData.append('main_image', mainFile);
         if (detectionMode === 'specific') {
-            formData.append('reference_image', referenceFile)
-            formData.append('confidence', confidenceThreshold)
+            formData.append('reference_image', referenceFile);
+            formData.append('confidence', confidenceThreshold);
         }
-        formData.append('bounding_box_threshold', boundingBoxThreshold)
-        formData.append('bb_color', selectedBBColor)
+        formData.append('bounding_box_threshold', boundingBoxThreshold);
+        formData.append('bb_color', selectedBBColor);
 
         // Set to null and processing while the image is being processed
-        setImageUrl('None')
-        setResultMessage('Processing...')
-        setBoundingBoxInfo([])
+        setImageUrl('None');
+        setResultMessage('Processing...');
+        setBoundingBoxInfo([]);
 
         const endpoint =
             detectionMode === 'all'
                 ? '/image/detect-all'
-                : '/image/detect-specific'
+                : '/image/detect-specific';
         try {
             const response = await fetch(`${baseURL}${endpoint}`, {
                 method: 'POST',
                 body: formData,
-            })
+            });
 
             if (response.ok) {
                 // Now fetch a json object
                 // Object contains the output image and bouding box information
-                const data = await response.json()
+                const data = await response.json();
                 // console.log(data)
                 // Save the image url from the data object.
-                setImageUrl(`data:image/jpeg;base64,${data.image}`)
+                setImageUrl(`data:image/jpeg;base64,${data.image}`);
                 // Save the bounding box information from the data object.
-                setBoundingBoxInfo(data.bounding_boxes)
-                setResultMessage('Processing completed successfully!')
+                setBoundingBoxInfo(data.bounding_boxes);
+                setResultMessage('Processing completed successfully!');
             } else {
-                setResultMessage('Processing failed. Try again.')
+                setResultMessage('Processing failed. Try again.');
             }
         } catch (error) {
-            setResultMessage('Error processing the image.')
-            console.error(error)
+            setResultMessage('Error processing the image.');
+            console.error(error);
         }
-    }
+    };
 
     return (
         <div className="">
             <header className="main-header">
                 <h1 className="text-2xl font-bold">TRAIT</h1>
                 <p className="text-lg">
-                    Trademark Identification & Analysis Tool
+                    Trademark Analysis & Identification Tool
                 </p>
             </header>
 
@@ -124,7 +124,7 @@ function App() {
                 />
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
